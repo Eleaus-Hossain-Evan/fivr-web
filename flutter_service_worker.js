c9e703b482599283dff6b951997c128c3e78896f
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2024-08-02 21:43:10 +0600

    change mouse cursor

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2163,14 +2163,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "ab6e65107ed2186ce8c4932684a8317e
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "9dc7e7218acb44d4c0f8167ff11c1cb6",
+"flutter_bootstrap.js": "d3e3a3fd4ac01332af91d00afccf7384",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "15b01bc977c5bdd57764ca1296f2e182",
 "/": "15b01bc977c5bdd57764ca1296f2e182",
-"main.dart.js": "707cf561739a121241fb72f8a8c99a6e",
+"main.dart.js": "ad8718886e0d721da26313758048104d",
 "manifest.json": "09fc07cffa520cd0fe17888a37dcc79a",
 "version.json": "22248edf1c6ef20ed9ae19bb4d8359c8"};
 // The application shell files that are downloaded before a service worker can
