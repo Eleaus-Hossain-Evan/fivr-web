bf439707669ab63aea2c7b38387747e64aa211d5
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2024-08-09 05:21:14 +0600

    tablet hero done...

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2549416254"
+    serviceWorkerVersion: "2013503375"
   }
 });
