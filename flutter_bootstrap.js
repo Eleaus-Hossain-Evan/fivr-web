5a45e709904ca1fbab3ea61b5b14176ded8446cd
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2024-08-09 03:34:28 +0600

    mobile complete

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "122382758"
+    serviceWorkerVersion: "2549416254"
   }
 });
